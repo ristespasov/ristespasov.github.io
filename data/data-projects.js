80b52bf0867de5401624788c05adce1248e7fa4d
Riste Spasov <ristespasov@hotmail.com>
2021-01-23 16:36:09 +0100

    data changed

--- data/data-projects.js
+++ data/data-projects.js
@@ -1,7 +1,7 @@
 var dataProjects = [
     {
         image: "../assets/projects/blog.png",
-        name: "BlogApp",
+        name: "Blog",
         description: "Blog web app developed as a final project for Harvard's CS50 course. Users can create accounts, reset passwords, write/update/delete blogs, update their profiles, read other user's posts, navigate through pages, etc.",
         technologies: "Django, SQL, Bootstrap 4",
         ghLink: "https://github.com/ristespasov/BlogApp",
@@ -13,7 +13,7 @@ var dataProjects = [
         description: "This web app provides data of total infected, recovered, active, and death cases from Coronavirus globally and by country. The data is presented with numbers, charts, and a map.",
         technologies: "React.js, Material UI",
         ghLink: "https://github.com/ristespasov/COVID19-tracker",
-        liveLink: "https://covid-19-tracker-3cf98.web.app/",
+        liveLink: "https://covid-19-tracker-2020project.netlify.app/",
     },
     {
         image: "../assets/projects/fastburger.png",
@@ -25,10 +25,10 @@ var dataProjects = [
     },
     {
         image: "../assets/projects/weather-air-check.png",
-        name: "Weather & Air check",
-        description: "An app with a responsive web design that renders great on a variety of devices and window or screen sizes. It provides data for current weather conditions, forecast and air quality index. Contains an auto location finder and allows a custom search.",
+        name: "Weathery",
+        description: "Weathery provides data for current weather conditions, forecast and air quality index. Contains an auto location finder and allows a custom search.",
         technologies: "React.js, CSS3, React Bootstrap 4",
         ghLink: "https://github.com/ristespasov/Weather-Air-check",
-        liveLink: "https://competent-elion-075159.netlify.app/",
+        liveLink: "https://weathery-19project.netlify.app/",
     }
 ]
